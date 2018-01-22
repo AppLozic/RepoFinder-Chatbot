d45a8581ad6e47da85f451aeb57caac9ec210301
Suraj Khanduri <surajkhanduri@voldemort.local>
2018-01-22 21:58:35 +0530

    updating config file

--- conf/config.js
+++ conf/config.js
@@ -1,7 +1,7 @@
 var config = {
   development: {
    brokerUrl:["https://apps.applozic.com"],
-    mongoDbUrl:"mongodb://admin:admin@localhost:27017/ApplozicBot?authSource=admin",
+    mongoDbUrl:"mongodb://<userName>:<password>@localhost:27017/RepoFinder?authSource=admin",
      applozicPlugin:{
       sendMessageUrl:"https://apps.applozic.com/rest/ws/message/v2/send",
       sendMailUrl:"https://apps.applozic.com/rest/ws/mail/send"
